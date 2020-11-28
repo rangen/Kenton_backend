3d411a8b25e3934a41998eb44f2427d75cac562d
Kenton Archer <kenton8654@yahoo.com>
2020-11-28 15:07:29 -0500

    making api/v1 controllers

--- app/controllers/api/v1/answers_controller.rb
+++ app/controllers/api/v1/answers_controller.rb
@@ -1,2 +1,7 @@
 class Api::V1::AnswersController < ApplicationController
+
+
+  def index
+    @answers = Answer.all
+  end
 end
--- app/controllers/api/v1/questions_controller.rb
+++ app/controllers/api/v1/questions_controller.rb
@@ -1,2 +1,38 @@
 class Api::V1::QuestionsController < ApplicationController
+
+  def index
+      @questions = Question.all
+      render json: @questions
+  end
+
+
+  def show
+      @question = Question.find(params[:id])
+      render json: @question, status: :ok
+  end
+
+  def create
+  #   @question = Question.new(question_params)
+  #   if @question.save
+  #     render json: @question, status: :ok
+  #   else
+  #     render json: {errors: @question.errors.full_messages}, status: :unproccesible_entity
+  #   end
+  # end
+  # creating one instance of question
+  # if not possible to save then render error message
+  end
+
+  private
+
+def question_params
+  params.require(:question).permit(:text)
+
+end
+
+
+
+
+
+
 end
