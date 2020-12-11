992b1a662f9e492eead0b2795e59060ee021774d
Kenton Archer <kenton8654@yahoo.com>
2020-12-10 21:47:29 -0500

    updating models w high_score

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,8 @@ require "rest-client"
 rm = RestClient.get 'https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=multiple'
 rm_array = JSON.parse(rm)["results"]
 
+Question.destroy_all
+
 rm_array.each do |question|
  Question.create(
  correct_anwser: question["correct_answer"],
